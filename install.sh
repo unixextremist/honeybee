2350a5867b8a97203c9d87f7a6440dc365bc9985
unixextremist <ysfmmekb@proton.me>
2026-05-21 13:28:31 +0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo xbps-install -Sy \
+  libstdc++ libgcc ncurses readline \
+  libX11 libxcb libXext libXrender libXft fontconfig freetype libxkbcommon \
+  gtk+3 glib pango cairo gdk-pixbuf qt5 \
+  libcurl libressl gnutls ca-certificates \
+  zlib bzip2 xz lz4 zstd brotli libpng libjpeg-turbo tiff libwebp \
+  dbus pam libcap icu-libs
